da86d2a7c6041ea4a970d29a07414ce5b9f0d378
Artyom Streltsov <112578642+Mortadren@users.noreply.github.com>
2024-02-01 13:38:55 +0700

    Update delete_from_folder.py

--- delete_from_folder.py
+++ delete_from_folder.py
@@ -1,32 +1,28 @@
 import os
 
 def delete_files(folder_path, target_string):
-    # Проверяем каждый файл в указанной папке
     for root, dirs, files in os.walk(folder_path):
         for file_name in files:
             if target_string in file_name:
                 file_path = os.path.join(root, file_name)
-                # Удаление файла, содержащего указанную строку в имени
                 os.remove(file_path)
                 print(f"Файл {file_path} удален.")
 
 def delete_folders(folder_path):
-    deleted_folders_count = 0  # Счетчик удаленных папок
+    deleted_folders_count = 0  
     
-    # Проверяем папки в обратном порядке для удаления пустых папок
+   
     for root, dirs, files in os.walk(folder_path, topdown=False):
         for folder_name in dirs:
             folder = os.path.join(root, folder_name)
-            if not os.listdir(folder):  # Если папка пуста
-                os.rmdir(folder)  # Удаляем пустую папку
+            if not os.listdir(folder):  
+                os.rmdir(folder) 
                 deleted_folders_count += 1
                 print(f"Папка {folder} удалена.")
     
     return deleted_folders_count
 
-# Укажите путь к папке, в которой нужно удалить файлы и пустые папки
 folder_to_search = 'C:/PROJECTS/development/folderOutput2'
-# Указываем строку, которая должна содержаться в имени файла для его удаления
 string_to_find = 'output-2'
 
 delete_files(folder_to_search, string_to_find)
