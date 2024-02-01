a61fba36748cba5d2076a090f37ff90acc25a969
Artyom Streltsov <112578642+Mortadren@users.noreply.github.com>
2024-02-01 13:37:49 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,23 +1,20 @@
 #  Подключение к redis
-HOST_REDIS = '192.168.55.4'
+HOST_REDIS = ''
 PORT_REDIS = 6379
 PASSWORD = ''
 
-# Подключение к ПЛК AS
-HOST_PLC_AS = "192.168.55.235" #  PLC 
+# Подключение к ПЛК 1
+HOST_PLC_AS = "" #  PLC 
 PORT_PLC_AS = 502
 
-# Подключение к ПЛК ES
-HOST_PLC_ES = "192.168.55.234" #  PLC 
+# Подключение к ПЛК 2
+HOST_PLC_ES = "" #  PLC 
 PORT_PLC_ES = 502
 
 # Регистры для чтения; [10, 2, True] где 10 - номер регистра, 2 - количество регистров, True - если тру, то значение флоат \100 
-READ_REG_AS = {'defect': [500, 1, False], 'control_status': [505, 1, False], 'counter_storage': [506, 1, False], 'green_com': [501, 1, False],
-               'yellow_com': [502, 1, False],'red_com': [503, 1, False], 'danger_red': [507, 1, False]
-            }
+READ_REG_AS = {}
 
-READ_REG_ES = {'frq_read': [2103, 1, True], 'volt_read': [2109, 1, False], 'cur_read': [2104, 1, False], 'speed': [1005, 2, False], 'lenght': [1001, 2, False]
-            }
+READ_REG_ES = {}
 
 # регистры для записи
 WRITE_REG_AS = {'defect': 500,
